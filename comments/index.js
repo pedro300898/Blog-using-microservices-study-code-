93f7f1f6fa3e732faf325b16f004e8e1dea8968e
João Pedro <pedro300898@gmail.com>
2021-05-03 19:00:56 -0300

    Create React App and fix Cors error

--- comments/index.js
+++ comments/index.js
@@ -1,28 +1,30 @@
 const express = require('express');
-const {randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) =>{
-    const commentId = randomBytes(4).toString('hex');
-    const {content} = req.body;
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
-    const comments = commentsByPostId[req.params.id] || [];
-    
-    comments.push({ id: commentId, content});
+  const comments = commentsByPostId[req.params.id] || [];
 
-    commentsByPostId[req.params.id] = comments;
+  comments.push({ id: commentId, content });
 
-    res.status(201).send(comments);
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
 });
 
-app.listen(4001, ()=>{
-    console.log('listening on 4001');
+app.listen(4001, () => {
+  console.log('Listening on 4001');
 });
