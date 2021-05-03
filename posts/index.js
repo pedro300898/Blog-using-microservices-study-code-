15d5850800eb6851b8c165718c8cef6817903379
João Pedro <pedro300898@gmail.com>
2021-05-03 16:40:19 -0300

    Route change

--- posts/index.js
+++ posts/index.js
@@ -1,20 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const {randomBytes} = require('crypto');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 const posts ={};
 
-app.get('/post', (req, res) => {
+app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/post', (req, res) => {
+app.post('/posts', (req, res) => {
     const id = randomBytes(4).toString('hex');
     const {title} = req.body;
 
-    post[id] = {
+    posts[id] = {
         id, title
     };
 
