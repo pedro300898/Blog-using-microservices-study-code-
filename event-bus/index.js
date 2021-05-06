f7988779c9e913eef495628d3a9b74932686178c
João Pedro <pedro300898@gmail.com>
2021-05-06 12:03:34 -0300

    Create Event Syncing

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
+
+const events = [];
 
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post('http://localhost:4000/events', event);
   axios.post('http://localhost:4001/events', event);
@@ -13,6 +18,10 @@ app.post('/events', (req, res) => {
   axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- moderation/index.js
+++ moderation/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.post('/events', async (req, res) => {
   const { type, data } = req.body;
